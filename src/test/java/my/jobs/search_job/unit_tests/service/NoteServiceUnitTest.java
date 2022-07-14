c5d8b611d04af8b8aa2ab3b3a4226315c4740e49
Rostmp <84409690+Rostmp@users.noreply.github.com>
2022-07-14 19:59:04 +0300

    add: unit-tests for service layer

---
+++ src/test/java/my/jobs/search_job/unit_tests/service/NoteServiceUnitTest.java
@@ -0,0 +1,69 @@
+package my.jobs.search_job.unit_tests.service;
+
+import my.jobs.search_job.model.Note;
+import my.jobs.search_job.repository.NoteRepository;
+import my.jobs.search_job.service.NoteService;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
+@SpringBootTest
+public class NoteServiceUnitTest {
+
+    @Mock
+    NoteRepository noteRepository;
+
+    @InjectMocks
+    NoteService noteService;
+
+    private final Note noteTest = mock(Note.class);
+
+    @Test
+    public void whenSaveNoteThenSuccess() {
+        Mockito.when(noteRepository.save(Mockito.any())).thenReturn(noteTest);
+        noteService.save(noteTest);
+        verify(noteRepository).save(noteTest);
+    }
+
+    @Test
+    public void whenFindByIdNoteThenSuccess() {
+        Mockito.when(noteRepository.findById(noteTest.getId())).thenReturn(Optional.of(noteTest));
+
+        Note actual = noteService.findById(noteTest.getId());
+
+        assertNotNull(actual);
+        assertEquals(noteTest, actual);
+        verify(noteRepository).findById(noteTest.getId());
+    }
+
+    @Test
+    public void whenGetAllNoteThenSuccess() {
+        List<Note> noteListTest = List.of(noteTest, noteTest, noteTest);
+        Mockito.when(noteRepository.findAll()).thenReturn(noteListTest);
+
+        List<Note> actual = noteService.getAllNote();
+
+        assertNotNull(actual);
+        assertEquals(noteListTest, actual);
+        verify(noteRepository).findAll();
+    }
+
+    @Test
+    public void whenDeleteNoteSuccess() {
+        Mockito.when(noteRepository.findById(noteTest.getId())).thenReturn(Optional.of(noteTest));
+        noteService.delete(noteTest.getId());
+        verify(noteRepository).findById(noteTest.getId());
+        verify(noteRepository).delete(noteTest);
+    }
+
+}
---
+++ src/test/java/my/jobs/search_job/unit_tests/service/ResponseServiceUnitTest.java
@@ -0,0 +1,139 @@
+package my.jobs.search_job.unit_tests.service;
+
+import my.jobs.search_job.model.Response;
+import my.jobs.search_job.repository.ResponseRepository;
+import my.jobs.search_job.service.ResponseService;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
+@SpringBootTest
+public class ResponseServiceUnitTest {
+
+    @Mock
+    ResponseRepository responseRepository;
+
+    @InjectMocks
+    ResponseService responseService;
+
+    private final Response responseTest = mock(Response.class);
+
+    @Test
+    public void whenAddResponseSuccess() {
+        Mockito.when(responseRepository.save(Mockito.any())).thenReturn(responseTest);
+        responseService.addResponse(responseTest);
+        verify(responseRepository).save(responseTest);
+    }
+
+    @Test
+    public void whenFindByIdResponseThenSuccess() {
+        Mockito.when(responseRepository.findById(responseTest.getId())).thenReturn(Optional.of(responseTest));
+
+        Response actual = responseService.findById(responseTest.getId());
+
+        assertNotNull(actual);
+        assertEquals(responseTest, actual);
+        verify(responseRepository).findById(responseTest.getId());
+    }
+
+    @Test
+    public void whenDeleteResponseByIdThenSuccess() {
+        Mockito.when(responseRepository.findById(responseTest.getId())).thenReturn(Optional.of(responseTest));
+        responseService.delete(responseTest.getId());
+        verify(responseRepository).findById(responseTest.getId());
+    }
+
+    @Test
+    public void whenResponseDoneArchiveFalseSortReverseSuccess() throws InterruptedException {
+        List<Response> responseListTest = new ArrayList<>();
+        for (int i = 0; i < 3; i++) {
+            responseListTest.add(new Response(String.valueOf(i)));
+            Thread.sleep(500);
+        }
+        Mockito.when(responseRepository.findResponseByDoneFalseAndArchiveFalse()).thenReturn(responseListTest);
+        List<Response> actual = responseService.findRespDoneArchiveFalse();
+        assertNotNull(actual);
+        assertEquals(new Response("2").getName(), actual.get(0).getName());
+        verify(responseRepository).findResponseByDoneFalseAndArchiveFalse();
+    }
+
+    @Test
+    public void whenResponseDoneTrueArchiveFalseSortReverseSuccess() throws InterruptedException {
+        List<Response> responseListTest = new ArrayList<>();
+        for (int i = 0; i < 3; i++) {
+            responseListTest.add(new Response(String.valueOf(i)));
+            Thread.sleep(500);
+        }
+        Mockito.when(responseRepository.findResponseByDoneTrueAndArchiveFalse()).thenReturn(responseListTest);
+        List<Response> actual = responseService.findRespDoneTrueArchiveFalse();
+        assertNotNull(actual);
+        assertEquals(new Response("2").getName(), actual.get(0).getName());
+        verify(responseRepository).findResponseByDoneTrueAndArchiveFalse();
+    }
+
+    @Test
+    public void whenResponseDoneFalseArchiveTrueSortReverseSuccess() throws InterruptedException {
+        List<Response> responseListTest = new ArrayList<>();
+        for (int i = 0; i < 3; i++) {
+            responseListTest.add(new Response(String.valueOf(i)));
+            Thread.sleep(500);
+        }
+        Mockito.when(responseRepository.findResponseByDoneFalseAndArchiveTrue()).thenReturn(responseListTest);
+        List<Response> actual = responseService.findRespDoneFalseArchiveTrue();
+        assertNotNull(actual);
+        assertEquals(new Response("2").getName(), actual.get(0).getName());
+        verify(responseRepository).findResponseByDoneFalseAndArchiveTrue();
+    }
+
+    @Test
+    public void whenUpdateStatusDoneThenSuccess() {
+        Response response = new Response("Test");
+        Mockito.when(responseRepository.findById(response.getId())).thenReturn(Optional.of(response));
+        Mockito.when(responseRepository.save(Mockito.any())).thenReturn(responseTest);
+
+        responseService.updateStatusDone(response.getId());
+
+        assertTrue(response.getDone());
+        verify(responseRepository).findById(responseTest.getId());
+        verify(responseRepository).save(Mockito.any());
+    }
+
+    @Test
+    public void whenUpdateStatusArchiveThenSuccess() {
+        Response response = new Response("Test");
+        Mockito.when(responseRepository.findById(response.getId())).thenReturn(Optional.of(response));
+        Mockito.when(responseRepository.save(Mockito.any())).thenReturn(responseTest);
+
+        responseService.updateStatusArchive(response.getId());
+
+        assertTrue(response.getArchive());
+        verify(responseRepository).findById(responseTest.getId());
+        verify(responseRepository).save(Mockito.any());
+    }
+
+    @Test
+    public void whenFindResponseByNameSortReverseSuccess() throws InterruptedException {
+        List<Response> responseListTest = new ArrayList<>();
+        for (int i = 0; i < 3; i++) {
+            responseListTest.add(new Response("test" + i));
+            Thread.sleep(500);
+        }
+        Mockito.when(responseRepository.searchResponseByNameLike("test")).thenReturn(responseListTest);
+        List<Response> actual = responseService.findResponseByName("test");
+        assertNotNull(actual);
+        assertEquals(new Response("test2").getName(), actual.get(0).getName());
+        verify(responseRepository).searchResponseByNameLike("test");
+    }
+
+
+}
